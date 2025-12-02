b577cf9392ffdc56b24da624495fab2736373079
TovillaFernando <fitl56@hotmail.com>
2025-12-02 17:26:31 -0600

    Update HeatMap0312.m

--- HeatMap0312.m
+++ HeatMap0312.m
@@ -1,16 +1,13 @@
-% Cargar datos desde el archivo Excel
 data = readtable('HeatMap.xlsx'); % Reemplaza con tu archivo
 
-% Definir las variables biomecánicas con sus nuevas etiquetas
 variables = {'Area', 'Longitud', 'VelocidadNormalizada', 'LongitudDeOndaNormalizada', 'AmplitudDeOndaNormalizada', 'Coleteos'};
 variableLabels = {'Área', 'Longitud', 'Velocidad normalizada', 'Longitud de onda normalizada', 'Amplitud de onda normalizada', 'Coleteos'}; % Nuevas etiquetas
 
+% Strains
 cepas = {'N2', 'ATM1', 'CZ26389', 'ATM1xCZ26389'}; % Orden deseado
 
-% Matriz para datos normalizados
 normalizedData = zeros(numel(cepas), numel(variables));
 
-% Normalizar todas las cepas respecto a N2
 controlCepa = 'N2';
 controlData = data(strcmp(data.Cepa, controlCepa), :);
 
@@ -21,26 +18,21 @@ for i = 1:numel(cepas)
     end
 end
 
-% Clustering jerárquico
-distances = pdist(normalizedData, 'euclidean'); % Distancias euclidianas
-linkages = linkage(distances, 'average'); % Agrupamiento promedio
+distances = pdist(normalizedData, 'euclidean');
+linkages = linkage(distances, 'average');
 
-% Reordenar el dendrograma y el mapa de calor al mismo orden
-[~, dendrogramOrder] = ismember(cepas, cepas); % Asegurar orden fijo
+[~, dendrogramOrder] = ismember(cepas, cepas);
 
-% Crear un mapa de calor con un colormap que va de verde a rojo
-nColors = 256; % Aumentar la cantidad de niveles para mayor gradualidad
+nColors = 256;
 
-% Colormap de verde a rojo
 greenToRedColormap = [linspace(0, 1, nColors)', linspace(1, 0, nColors)', zeros(nColors, 1)];
 
-% Etiquetas personalizadas para las cepas
+% Label strains
 cepaLabels = {'WT', ...
     '\it{-mctp-1}', ...     % En cursiva para ATM1
     '\it{-esyt-2}', ...     % En cursiva para CZ26389
     '-mctp-1/-esyt-2'};    % Para ATM1xCZ26389
 
-% Representar dendrograma y heatmap en la misma figura
 figure;
 
 % Subplot 1: Dendrograma
@@ -54,6 +46,4 @@ subplot(1, 2, 2);
 heatmap(variableLabels, cepaLabels, normalizedData(dendrogramOrder, :), ...
     'Colormap', greenToRedColormap, 'ColorLimits', [0, 0.5], 'CellLabelColor', 'none');
 
-% Eliminar el título en el mapa de calor
-% title('Mapa de calor de parámetros normalizados'); % Esta línea ha sido eliminada
 colorbar;
